887bbd79bc933c4045271b5d148d2260a9307936
Victor Bello <imuetinyanbello@gmail.com>
2024-06-23 08:09:22 +0100

    added insert many and delete one endpoint

--- src/app/api/docs/route.ts
+++ src/app/api/docs/route.ts
@@ -40,3 +40,107 @@ export async function GET(req: NextRequest, res: NextResponse) {
     );
   }
 }
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "Error: No signed in user" },
+        { status: 401 }
+      );
+    }
+
+    const client = await clientPromise;
+    const db = client.db(dbName);
+
+    const { docs }: { docs: Scanned_docs[] } = await req.json();
+
+    if (!Array.isArray(docs)) {
+      return NextResponse.json(
+        { error: "Docs Must be an array" },
+        { status: 400 }
+      );
+    }
+
+    const options = { ordered: true };
+
+    let docs_re: Scanned_docs[] = [];
+
+    for (let d of docs) {
+      const findDoc = await db
+        .collection(document_names.scanned_docs)
+        .findOne({ transaction_id: d.transaction_id });
+
+      if (findDoc) {
+        return NextResponse.json(
+          { error: "Duplicate transaction id" },
+          { status: 400 }
+        );
+      }
+
+      const scanned_d = {
+        ...d,
+        date: new Date(d["date"]),
+        user_id: userId,
+        created_at: new Date(),
+      };
+
+      docs_re.push(scanned_d);
+    }
+
+    const result =
+      docs_re.length > 0 &&
+      (await db
+        .collection(document_names.scanned_docs)
+        .insertMany(docs_re, options));
+
+    if (!result) {
+      throw new Error("insertion failed");
+    }
+
+    return NextResponse.json({
+      result,
+      status: true,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const client = await clientPromise;
+    const db = client.db(dbName);
+
+    const { id } = await req.json();
+
+    if (!id) {
+      return NextResponse.json({ error: "id is required" }, { status: 400 });
+    }
+
+    const deletedDoc = await db
+      .collection(document_names.scanned_docs)
+      .deleteOne({
+        id,
+      });
+
+    return NextResponse.json({
+      message: `doc deleted`,
+      status: deletedDoc.acknowledged,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error,
+      },
+      { status: 500 }
+    );
+  }
+}
