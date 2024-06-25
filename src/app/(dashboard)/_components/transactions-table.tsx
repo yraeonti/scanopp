e5dbc6f123b213b6de18f3b7bd81c17abda67dee
Oscar Okereke <oscarokereke@Oscars-MacBook-Air.local>
2024-06-25 10:26:03 +0100

    fix: bug

--- src/app/(dashboard)/_components/transactions-table.tsx
+++ src/app/(dashboard)/_components/transactions-table.tsx
@@ -1,12 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DataTable } from "@/components/ui/data-table";
-import { transactions } from "@/constans/constants";
+
 import { transactions_columns } from "./transactions-columns";
 import { Transaction } from "@/types";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 
 export const TransactionsTable = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const { isLoading, data, isError } = useGetTransactions();
+
+  useEffect(() => {
+    if (data && data?.length > 0) {
+      setTransactions(data as Transaction[]);
+    }
+  }, [data]);
   return (
     <Card>
       <CardHeader>
@@ -23,6 +33,7 @@ export const TransactionsTable = () => {
           data={transactions as Transaction[]}
           columns={transactions_columns}
           searchKey="transaction_name"
+          loading={isLoading}
         />
       </CardContent>
     </Card>
